1be234926511eee933d25b0879c45aa031e6f0c1
ASA <asa.mohammed22@gmail.com>
2021-05-25 09:23:51 -0400

    Finishes Display option button

--- spoty_excuvator_UI.py
+++ spoty_excuvator_UI.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 import sys
 
 import json
@@ -13,92 +14,112 @@ class AppWindow(QMainWindow):
         # Calls super constructor
         super(AppWindow, self).__init__()
 
-        # Creates screen variables
-        self.window_title: str = "Spoty Excuvator"
-        self.screen_offset_x: int = 150
-        self.screen_offset_y: int = 150     
-        self.screen_x: int = 900
-        self.screen_y: int = 600
+        # Screen variables
+        self.WINDOW_TITLE: str = "Spoty Excuvator"
+        self.SCREEN_OFFSET_X: int = 150
+        self.SCREEN_OFFSET_Y: int = 150     
+        self.SCREEN_X: int = 900
+        self.SCREEN_Y: int = 600
+
+        # Avoids magic numbers
+        self.CBX_OPTION_ARTIST: int = 0
+        self.CBX_OPTION_TRACK_ARTIST: int = 1
+        self.CBX_OPTION_ARTIST_TRACK: int = 2
+
+        # Directory Button variables
+        self.DIRECTORY_BUTTON_Y: int = 50
+
+        # Checkbox variables
+        self.CBX_X: int = 200
+        self.CBX_Y_1: int = 150
+        self.CBX_Y_2: int = 180
+        self.CBX_Y_3: int = 210
+        self.CBX_WIDTH: int = 100
+        self.CBX_HEIGHT: int = 30
+
 
         # Calls UI method
         self.initUI()
         
 
     def initUI(self):
-        # Sets up screen
-        self.setGeometry(self.screen_offset_x, self.screen_offset_y, self.screen_x, self.screen_y)
-        self.setWindowTitle(self.window_title)
+        # Setup screen
+        self.setGeometry(self.SCREEN_OFFSET_X, self.SCREEN_OFFSET_Y, self.SCREEN_X, self.SCREEN_Y)
+        self.setWindowTitle(self.WINDOW_TITLE)
         
-        self.directory_button = QPushButton('Select Folder', self)
+
+        # Setup select folder button
+        self.directory_button = QPushButton('Select Downloaded Spotify Data Folder', self)
+        self.directory_button.setFont(QFont('Sans-serif', 18))
         self.directory_button.adjustSize()
-        self.directory_button.move(int((self.screen_x*.5) - (self.directory_button.size().width()*.5)), 50)
+        self.directory_button.move(int((self.SCREEN_X*.5) - (self.directory_button.size().width()*.5)), self.DIRECTORY_BUTTON_Y)
         self.directory_button.clicked.connect(self.select_file_btn)
+        
 
-
-
-
-        # creating check box
+        # Creates 3 checkboxes
         self.option1_cbx = QCheckBox("Most Played Artist", self)
-        self.option1_cbx.setGeometry(200, 150, 100, 30)
+        self.option1_cbx.setGeometry(self.CBX_X, self.CBX_Y_1, self.CBX_WIDTH, self.CBX_HEIGHT)
+        self.option2_cbx = QCheckBox("Artist then Track", self)
+        self.option2_cbx.setGeometry(self.CBX_X, self.CBX_Y_2, self.CBX_WIDTH, self.CBX_HEIGHT)
+        self.option3_cbx = QCheckBox("Track then Artist", self)
+        self.option3_cbx.setGeometry(self.CBX_X, self.CBX_Y_3, self.CBX_WIDTH, self.CBX_HEIGHT)
   
-        # creating check box
-        self.option2_cbx = QCheckBox("Geek", self)
-        self.option2_cbx.setGeometry(200, 180, 100, 30)
-  
-        # creating check box
-        self.option3_cbx = QCheckBox(" Not a geek ?", self)
-        self.option3_cbx.setGeometry(200, 210, 100, 30)
-  
-        # calling the uncheck method if any check box state is changed
+
+        # Calls update method to uncheck other boxes
         self.option1_cbx.stateChanged.connect(self.cbx_update)
         self.option2_cbx.stateChanged.connect(self.cbx_update)
         self.option3_cbx.stateChanged.connect(self.cbx_update)
-  
 
-    # uncheck method
+
+        # Setup Display button
+        self.directory_button = QPushButton('Display Data', self)
+        self.directory_button.setFont(QFont('Times', 20))
+        self.directory_button.adjustSize()
+        self.directory_button.move(int((self.SCREEN_X*.5) - (self.directory_button.size().width()*.5)), 350)
+        self.directory_button.clicked.connect(self.file_calculate)
+
+
     def cbx_update(self, state):
-  
-        # checking if state is checked
+        # Checks if state is changed
         if state == Qt.Checked:
   
-            # if first check box is selected
+            # If cbx 1 is selected
             if self.sender() == self.option1_cbx:
   
-                # making other check box to uncheck
+                # Unchecks other boxes
                 self.option2_cbx.setChecked(False)
                 self.option3_cbx.setChecked(False)
 
-                self.selected_method = 0
+                # Sets method selector variable
+                self.selected_method = self.CBX_OPTION_ARTIST
   
-            # if second check box is selected
+            # If cbx 2 is selected
             elif self.sender() == self.option2_cbx:
   
-                # making other check box to uncheck
+                # Unchecks other boxes
                 self.option1_cbx.setChecked(False)
                 self.option3_cbx.setChecked(False)
   
-                self.selected_method = 1
+                # Sets method selector variable
+                self.selected_method = self.CBX_OPTION_TRACK_ARTIST
 
-            # if third check box is selected
+            # If cbx 3 is selected
             elif self.sender() == self.option3_cbx:
   
-                # making other check box to uncheck
+                # Unchecks other boxes
                 self.option1_cbx.setChecked(False)
                 self.option2_cbx.setChecked(False)
 
-                self.selected_method = 2
+                # Sets method selector variable
+                self.selected_method = self.CBX_OPTION_ARTIST_TRACK
 
 
     def select_file_btn(self):
         # Lets user select a path to data folder
         self.selected_directory_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Spotify Data Folder')
 
-        # Calls the functions to do magic
-        self.file_calculate()
-
 
     def file_calculate(self):
-        
         # Select which data to print
         METHOD = self.selected_method
         # 0: Artist 		(Most played artist)
@@ -126,9 +147,9 @@ class AppWindow(QMainWindow):
 
             # Append based on the selected choice
             for i in data:
-                if METHOD == 0: myList.append(i["artistName"])
-                elif METHOD == 1: myList.append(f"{i['trackName']}  ---  {i['artistName']}")
-                elif METHOD == 2: myList.append(f"{i['artistName']}  ---  {i['trackName']}")
+                if METHOD == self.CBX_OPTION_ARTIST: myList.append(i["artistName"])
+                elif METHOD == self.CBX_OPTION_TRACK_ARTIST: myList.append(f"{i['trackName']}  ---  {i['artistName']}")
+                elif METHOD == self.CBX_OPTION_ARTIST_TRACK: myList.append(f"{i['artistName']}  ---  {i['trackName']}")
 
 
         # Counts how many of each song is played
@@ -145,7 +166,6 @@ class AppWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-
     # Starts app process
     app = QApplication(sys.argv)
     # Creates instance of the class
