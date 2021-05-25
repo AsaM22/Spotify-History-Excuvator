45839c0b85edd71317144cc111be22baf82f0af1
ASA <asa.mohammed22@gmail.com>
2021-05-25 09:55:19 -0400

    Starts time per artist

--- spoty_excuvator_UI.py
+++ spoty_excuvator_UI.py
@@ -4,9 +4,10 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 import sys
 
-import json
+from json import load
 from collections import Counter
-import glob
+from glob import glob1
+
 
 
 class AppWindow(QMainWindow):
@@ -25,18 +26,22 @@ class AppWindow(QMainWindow):
         self.CBX_OPTION_ARTIST: int = 0
         self.CBX_OPTION_TRACK_ARTIST: int = 1
         self.CBX_OPTION_ARTIST_TRACK: int = 2
+        self.CBX_OPTION_TOTAL_ARTIST_TIME: int = 3
 
-        # Directory Button variables
+        # Directory button variables
         self.DIRECTORY_BUTTON_Y: int = 50
 
         # Checkbox variables
         self.CBX_X: int = 200
         self.CBX_Y_1: int = 150
-        self.CBX_Y_2: int = 180
-        self.CBX_Y_3: int = 210
-        self.CBX_WIDTH: int = 100
+        self.CBX_Y_2: int = self.CBX_Y_1 + 30
+        self.CBX_Y_3: int = self.CBX_Y_2 + 30
+        self.CBX_Y_4: int = self.CBX_Y_3 + 30
+        self.CBX_WIDTH: int = 150
         self.CBX_HEIGHT: int = 30
 
+        # Display button variables
+        self.DISPLAY_BUTTON_Y: int = 400
 
         # Calls UI method
         self.initUI()
@@ -63,6 +68,9 @@ class AppWindow(QMainWindow):
         self.option2_cbx.setGeometry(self.CBX_X, self.CBX_Y_2, self.CBX_WIDTH, self.CBX_HEIGHT)
         self.option3_cbx = QCheckBox("Track then Artist", self)
         self.option3_cbx.setGeometry(self.CBX_X, self.CBX_Y_3, self.CBX_WIDTH, self.CBX_HEIGHT)
+        self.option4_cbx = QCheckBox("Time per Artist", self)
+        self.option4_cbx.setGeometry(self.CBX_X, self.CBX_Y_4, self.CBX_WIDTH, self.CBX_HEIGHT)
+
   
         # Calls update method to uncheck other boxes
         self.option1_cbx.stateChanged.connect(self.cbx_update)
@@ -74,7 +82,7 @@ class AppWindow(QMainWindow):
         self.display_button = QPushButton('Display Data', self)
         self.display_button.setFont(QFont('Times', 20))
         self.display_button.adjustSize()
-        self.display_button.move(int((self.SCREEN_X*.5) - (self.display_button.size().width()*.5)), 350)
+        self.display_button.move(int((self.SCREEN_X*.5) - (self.display_button.size().width()*.5)), self.DISPLAY_BUTTON_Y)
         self.display_button.clicked.connect(self.file_calculate)
 
 
@@ -88,6 +96,7 @@ class AppWindow(QMainWindow):
                 # Unchecks other boxes
                 self.option2_cbx.setChecked(False)
                 self.option3_cbx.setChecked(False)
+                self.option4_cbx.setChecked(False)
 
                 # Sets method selector variable
                 self.selected_method = self.CBX_OPTION_ARTIST
@@ -98,6 +107,7 @@ class AppWindow(QMainWindow):
                 # Unchecks other boxes
                 self.option1_cbx.setChecked(False)
                 self.option3_cbx.setChecked(False)
+                self.option4_cbx.setChecked(False)
   
                 # Sets method selector variable
                 self.selected_method = self.CBX_OPTION_TRACK_ARTIST
@@ -108,10 +118,22 @@ class AppWindow(QMainWindow):
                 # Unchecks other boxes
                 self.option1_cbx.setChecked(False)
                 self.option2_cbx.setChecked(False)
+                self.option4_cbx.setChecked(False)
 
                 # Sets method selector variable
                 self.selected_method = self.CBX_OPTION_ARTIST_TRACK
+            
+            # If cbx 4 is selected
+            elif self.sender() == self.option4_cbx:
+  
+                # Unchecks other boxes
+                self.option1_cbx.setChecked(False)
+                self.option2_cbx.setChecked(False)
+                self.option3_cbx.setChecked(False)
 
+                # Sets method selector variable
+                self.selected_method = self.CBX_OPTION_TOTAL_ARTIST_TIME
+            
 
     def select_file_btn(self):
         # Lets user select a path to data folder
@@ -119,18 +141,18 @@ class AppWindow(QMainWindow):
 
 
     def file_calculate(self):
-        # Select which data to print
-        METHOD = self.selected_method
         # 0: Artist 		(Most played artist)
         # 1: Track/Artist 	(Track then Artist)
         # 2: Artist/Tarck	(Artist then Track)
 
-
         # Creates a list to append all the songs to
         myList = list()
 
+        # Creates a dict for sorted data
+        sorted_data = dict()
+
         # Finds the total number of StreamingHistory files
-        file_count = len(glob.glob1(self.selected_directory_path, "StreamingHistory*.json"))
+        file_count = len(glob1(self.selected_directory_path, "StreamingHistory*.json"))
 
 
         # Loops through the amout of files you have (example: 4 loops)
@@ -141,25 +163,28 @@ class AppWindow(QMainWindow):
 
             # Gets the raw json data
             with open(_file_path, encoding="utf8") as f:
-                data = json.load(f)
+                data = load(f)
 
 
             # Append based on the selected choice
-            if METHOD == self.CBX_OPTION_ARTIST:
+            if self.selected_method == self.CBX_OPTION_ARTIST:
                 for i in data:
                     myList.append(i["artistName"])
-            elif METHOD == self.CBX_OPTION_TRACK_ARTIST:
+            elif self.selected_method == self.CBX_OPTION_TRACK_ARTIST:
                 for i in data:
                     myList.append(f"{i['trackName']}  ---  {i['artistName']}")
-            elif METHOD == self.CBX_OPTION_ARTIST_TRACK:
+            elif self.selected_method == self.CBX_OPTION_ARTIST_TRACK:
                 for i in data:
                     myList.append(f"{i['artistName']}  ---  {i['trackName']}")
+            elif self.selected_method == self.CBX_OPTION_TOTAL_ARTIST_TIME:
+                for i in data:
+                    pass
 
 
         # Counts how many of each song is played
         counted_data = Counter(myList)
 
-        # Sorts the list (asending)
+        # Sorts the dict (asending)
         sorted_data = {k: v for k, v in sorted(counted_data.items(), key=lambda item: item[1])}
 
 
