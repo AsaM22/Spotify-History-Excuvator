4d3f75a4e2b9d50f24e7a94220a56a5fa4cff7c5
ASA <asa.mohammed22@gmail.com>
2021-05-25 08:39:44 -0400

    Changes variable name from sum to total_listened

--- total_time_listened.py
+++ total_time_listened.py
@@ -4,28 +4,28 @@ import json
 def main():
 
     # File Path to Streaming History
-    _file_path = "StreamingHistory0.json"
+    _file_path = "TestData/StreamingHistory0.json"
 
-    # Initalize filepath
-    sum = 0
+    # Initalize total
+    total_listened = 0
 
     # Gets the raw json data
-    with open(_file_path, "r") as f:
+    with open(_file_path, "r", encoding="utf8") as f:
         data = json.load(f)
 
-        # Loop through data and add to sum
+        # Loop through data and add to total_listened
         for i in data:
-            sum += i["msPlayed"]
+            total_listened += i["msPlayed"]
 
     # Display results 
     print("You've listened for:")
-    print(sum, "ms")
-    print('{:.2f}'.format(sum/1000), "Seconds")
-    print('{:.2f}'.format(sum/60000), "Mins")
-    print('{:.2f}'.format(sum/3600000), "Hours")
-    print('{:.2f}'.format(sum/86400000), "Days")
-    print('{:.2f}'.format(sum/604800000), "Weeks")
-    print('{:.2f}'.format(sum/2628000000), "Months")
+    print(total_listened, "ms")
+    print('{:.2f}'.format(total_listened/1000), "Seconds")
+    print('{:.2f}'.format(total_listened/60000), "Mins")
+    print('{:.2f}'.format(total_listened/3600000), "Hours")
+    print('{:.2f}'.format(total_listened/86400000), "Days")
+    print('{:.2f}'.format(total_listened/604800000), "Weeks")
+    print('{:.2f}'.format(total_listened/2628000000), "Months")
 
 
 if __name__ == "__main__":
